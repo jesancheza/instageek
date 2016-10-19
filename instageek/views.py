50f7c6aa3c004df4fd69cda34d1558b720186705
Jose Enrique Sanchez Alfonso <jesanchez@jesanchez.es>
2016-10-19 23:48:37 +0200

    Internacionalización y localización con fallo en idioma por defecto

---
+++ instageek/views.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from django.shortcuts import render
+from django.utils import translation
+
+__author__ = 'joseenriquesanchezalfonso'
+
+
+def hello(request):
+    forced_language = request.GET.get('lang')
+    if forced_language:
+        translation.activate(forced_language)
+
+    session_language = request.session.get('lang')
+    if session_language:
+        translation.activate(session_language)
+
+    return render(request, 'hello.html', {"name": request.GET.get("name", "John Doe")})
