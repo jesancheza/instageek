5677f09e6b6cc41da5faaedb7933b63d0c933a8a
Jose Enrique Sanchez Alfonso <jesanchez@jesanchez.es>
2016-10-20 21:54:59 +0200

    Cambiamos el campo image del modelo a ThumbnailerImageField

--- posts/models.py
+++ posts/models.py
@@ -2,12 +2,13 @@ from __future__ import unicode_literals
 
 from django.contrib.auth.models import User
 from django.db import models
+from easy_thumbnails.fields import ThumbnailerImageField
 
 
 class Post(models.Model):
 
     owner = models.ForeignKey(User)
     description = models.TextField()
-    image = models.ImageField()
+    image = ThumbnailerImageField(resize_source={"size": (500, 500)})
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
