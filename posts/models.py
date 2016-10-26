3cb65d6751dd3fc49893cc44209052497d528330
Jose Enrique Sanchez Alfonso <jesanchez@jesanchez.es>
2016-10-27 00:16:32 +0200

    Add internacionalization model post

--- posts/models.py
+++ posts/models.py
@@ -1,14 +1,20 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import ugettext as _
 
 
 class Post(models.Model):
 
     owner = models.ForeignKey(User)
-    description = models.TextField()
-    image = models.ImageField()
+    description = models.TextField(_("Description"))
+    image = models.ImageField(_("Image"))
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
-    image_resized = models.BooleanField(default=False)
+    image_resized = models.BooleanField(_("Image Resized"), default=False)
+
+    class Meta:
+        verbose_name = _('Post')
+        verbose_name_plural = _('Posts')
